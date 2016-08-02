536dd3969681a7c573dfd2aab30beaaf1cb50bc3
CCEIDT <jaime.rios@colombiacompra.gov.co>
2016-08-02 12:07:28 -0500

    host interno

--- opendata/settings.py
+++ opendata/settings.py
@@ -100,7 +100,7 @@ AUTHENTICATION_BACKENDS = (
 SESSION_ENGINE = 'mongoengine.django.sessions'
 SESSION_SERIALIZER = 'mongoengine.django.sessions.BSONSerializer'
 from mongoengine import connect
-connect(db='opendataocid', alias='default', port=5017)
+connect(db='opendataocid', alias='default', port=5017, host='cceproocds020.compute-a18530.oraclecloud.internal')
 
 
 
