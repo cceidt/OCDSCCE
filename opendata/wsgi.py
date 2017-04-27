044b3cb7140497f6c5e7735c6161490247c35e24
juandavidvallejo <juandavidvallejo@gmail.com>
2017-04-27 10:58:47 -0500

    Update wsgi.py
    Se cambia el archivo wsgi

--- opendata/wsgi.py
+++ opendata/wsgi.py
@@ -7,8 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
-
+import os, sys
+sys.path.append('/app/ocds')
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opendata.settings")
